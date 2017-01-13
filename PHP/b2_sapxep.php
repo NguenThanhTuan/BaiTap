735fae57b96d9c84e6df1ee094725f1bd8971198
NguenThanhTuan <thanhtuannc95@gmail.com>
2017-01-13 09:34:18 +0700

    Update b2_sapxep.php

--- PHP/b2_sapxep.php
+++ PHP/b2_sapxep.php
@@ -1,35 +1,65 @@
 <?php 
-	echo "Vòng lặp for". "<br>";
-	for ($i = 0; $i < 5; $i++)
-	{
-		$array[] =  rand(1, 30);
-	}
-	sort($array);
-	echo "Sắp xếp tăng dần: ";
-	print_r("<pre>");
-	print_r($array);
+	//Tăng dần FOR
+    function sapxeptang_for($mang)
+    {
+        $sophantu = count($mang);
+        for ($i = 0; $i < ($sophantu - 1); $i++)
+        {
+            for ($j = $i+1; $j < $sophantu; $j++)
+            {
+                if ($mang[$i] > $mang[$j])
+                {
+                    $tmp = $mang[$j];
+                    $mang[$j] = $mang[$i];
+                    $mang[$i] = $tmp;
+                }
+            }
+        }
+        return $mang;
+    }
 
-	rsort($array);
-	echo "<br>". "Sắp xếp giảm dần: ";
-	print_r ("<pre>");
-	print_r($array);
-		
-	echo "<br>". "Vòng lặp while". "<br>";
-	$i = 0;
-	while ($i < 6) 
-	{
-		$i++;
-		$array1[] = rand(1, 30);
-	}
-	sort($array1);
-	echo "Sắp xếp tăng dần: ";
-	print_r("<pre>");
-	print_r($array1);
+    //Giảm dần FOR
+    function sapxepgiam_for($mang)
+    {
+        $sophantu = count($mang);
+        for ($i = 0; $i < ($sophantu - 1); $i++)
+        {
+            for ($j = $i+1; $j < $sophantu; $j++)
+            {
+                if ($mang[$i] < $mang[$j])
+                {
+                    $tmp = $mang[$j];
+                    $mang[$j] = $mang[$i];
+                    $mang[$i] = $tmp;
+                }
+            }
+        }
+        return $mang;
+    }
 
-	rsort($array1);
-	echo "<br>". "Sắp xếp giảm dần: ";
-	print_r ("<pre>");
-	print_r($array1);
+    //Hiển thị FOR
+    function hienthi_for($mang)
+    {
+        $sophantu = count($mang);
+        for ($i = 0; $i < $sophantu; $i++) 
+        { 
+            print_r("<pre>");
+            echo $mang[$i]. ' ';
+        }
+    }
+
+    //main
+    echo "Sắp xếp tăng_for". "<br>";
+    $mang = array(3, 27, 9, 11, 94, 95, 30);
+    $mang = sapxeptang_for($mang);
+    hienthi_for($mang);
+    echo "<br>". "Sắp xếp giảm_for";
+    $mang2 = array(3, 27, 9, 11, 94, 95, 30);
+    $mang2 = sapxepgiam_for($mang2);
+    hienthi_for($mang2);
+
+	
+
 
 	echo "Vòng lặp do...while". "<br>";
 	$i = 0;
